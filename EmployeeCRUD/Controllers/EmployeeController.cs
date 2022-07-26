c893f4ef43abbe3ec32c7462964b8fd3bdaf27d2
jagadeesh0731 <52787121+jagadeesh0731@users.noreply.github.com>
2022-07-26 16:07:03 +0530

    My First Commit

---
+++ EmployeeCRUD/Controllers/EmployeeController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using EmployeeCRUD.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeCRUD.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public EmployeeController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var displaydata = _db.Employee.ToList();
+            return View(displaydata);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(NewEmpClass nec)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Add(nec);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(nec);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if(id==null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var getempdetails = await _db.Employee.FindAsync(id);
+            return View(getempdetails);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(NewEmpClass nc)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Update(nc);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(nc);
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var getempdetails = await _db.Employee.FindAsync(id);
+            return View(getempdetails);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var getempdetails = await _db.Employee.FindAsync(id);
+            return View(getempdetails);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            
+            var getempdetails = await _db.Employee.FindAsync(id);
+            _db.Employee.Remove(getempdetails);
+            _db.Employee.Remove(getempdetails);
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ EmployeeCRUD/Models/NewEmpClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeCRUD.Models
+{
+    public class NewEmpClass
+    {
+        [Key]
+        public int EmployeeID { get; set; }
+
+        [Required(ErrorMessage ="Enter Employee Name")]
+        [Display(Name ="Employee Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage ="Enter Employee Position")]
+        [Display(Name="Position")]
+        public string Position { get; set; }
+
+        [Required(ErrorMessage = "Enter Employee Office")]
+        [Display(Name = "Office")]
+        public string Office { get; set; }
+
+        [Required(ErrorMessage = "Enter Employee Age")]
+        [Display(Name = "Age")]
+        [Range(20,50)]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Enter Employee Salary")]
+        [Display(Name = "Salary")]
+        public int Salary { get; set; }
+    }
+}
